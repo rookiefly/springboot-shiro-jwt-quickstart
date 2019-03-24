599ea7bf33214d07d0350cc5cde3e85a00c9155a
rookiefly <rookiefly@163.com>
2019-03-24 18:02:06 +0800

    add swagger ui switch

--- src/main/java/com/rookiefly/springboot/helloworld/config/Swagger2Config.java
+++ src/main/java/com/rookiefly/springboot/helloworld/config/Swagger2Config.java
@@ -1,5 +1,6 @@
 package com.rookiefly.springboot.helloworld.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -14,9 +15,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class Swagger2Config {
 
+    @Value("${swagger.show}")
+    private boolean swaggerShow;
+
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .enable(swaggerShow)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.rookiefly.springboot.helloworld.controller"))
