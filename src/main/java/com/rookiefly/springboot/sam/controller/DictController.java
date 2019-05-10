91c544dc57fbd4c66e89bbd4ae0caca402aeca37
rookiefly <rookiefly@163.com>
2019-05-10 10:39:45 +0800

    数据字典

--- src/main/java/com/rookiefly/springboot/sam/controller/DictController.java
+++ src/main/java/com/rookiefly/springboot/sam/controller/DictController.java
@@ -1,9 +1,12 @@
 package com.rookiefly.springboot.sam.controller;
 
+import com.rookiefly.springboot.sam.mapper.dict.DictMapper;
 import com.rookiefly.springboot.sam.model.CommonResponse;
 import com.rookiefly.springboot.sam.model.dict.Dictionary;
 import com.rookiefly.springboot.sam.model.dict.DictionaryCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +17,9 @@ import java.util.HashMap;
 @RequestMapping("/dict")
 public class DictController {
 
+    @Autowired
+    private DictMapper dictMapper;
+
     @GetMapping("/list")
     public CommonResponse queryDictionaryList() {
         CommonResponse successResponse = CommonResponse.newSuccessResponse();
@@ -21,6 +27,15 @@ public class DictController {
         successResponse.setData(data);
         data.put("dictionaryList", new ArrayList<Dictionary>());
         data.put("dictionaryCategoryList", new ArrayList<DictionaryCategory>());
+        return successResponse;
+    }
+
+    @GetMapping("/list/{categoryId}")
+    public CommonResponse queryDictionaryListByCategoryId(@PathVariable("categoryId") Long categoryId) {
+        CommonResponse successResponse = CommonResponse.newSuccessResponse();
+        HashMap<Object, Object> data = new HashMap<>();
+        successResponse.setData(data);
+        data.put("dictionaryList", dictMapper.queryDictionaryByCategoryId(categoryId));
         return successResponse;
     }
 }
--- src/main/java/com/rookiefly/springboot/sam/mapper/dict/DictMapper.java
+++ src/main/java/com/rookiefly/springboot/sam/mapper/dict/DictMapper.java
@@ -1,8 +1,25 @@
 package com.rookiefly.springboot.sam.mapper.dict;
 
+import com.rookiefly.springboot.sam.model.dict.Dictionary;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface DictMapper {
+
+    /**
+     * id查询字典
+     * @param id
+     * @return
+     */
+    Dictionary queryDictionaryById(Long id);
+
+    /**
+     * 分类id查询字典列表
+     * @param categoryId
+     * @return
+     */
+    List<Dictionary> queryDictionaryByCategoryId(Long categoryId);
 
 }
